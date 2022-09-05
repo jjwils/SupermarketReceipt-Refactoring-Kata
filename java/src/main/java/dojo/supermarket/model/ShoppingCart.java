b280c0f2f15f10f9dc7521c0040e9f0e7d3de46b
jjwils <mail@johnwilson.me.uk>
2022-09-05 19:16:20 +0100

    covered and refactored

--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -15,10 +15,6 @@ public class ShoppingCart {
         return new ArrayList<>(items);
     }
 
-    void addItem(Product product) {
-        this.addItemQuantity(product, 1.0);
-    }
-
     Map<Product, Double> productQuantities() {
         return productQuantities;
     }
@@ -35,43 +31,74 @@ public class ShoppingCart {
 
     void handleOffers(Receipt receipt, Map<Product, Offer> offers, SupermarketCatalog catalog) {
         for (Product p: productQuantities().keySet()) {
-            double quantity = productQuantities.get(p);
-            if (offers.containsKey(p)) {
-                Offer offer = offers.get(p);
-                double unitPrice = catalog.getUnitPrice(p);
-                int quantityAsInt = (int) quantity;
-                Discount discount = null;
-                int x = 1;
-                if (offer.offerType == SpecialOfferType.ThreeForTwo) {
-                    x = 3;
-
-                } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
-                    x = 2;
-                    if (quantityAsInt >= 2) {
-                        double total = offer.argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
-                        double discountN = unitPrice * quantity - total;
-                        discount = new Discount(p, "2 for " + offer.argument, -discountN);
-                    }
-
-                } if (offer.offerType == SpecialOfferType.FiveForAmount) {
-                    x = 5;
-                }
-                int numberOfXs = quantityAsInt / x;
-                if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-                    double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
-                    discount = new Discount(p, "3 for 2", -discountAmount);
-                }
-                if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
-                    discount = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
-                }
-                if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-                    double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                    discount = new Discount(p, x + " for " + offer.argument, -discountTotal);
-                }
-                if (discount != null)
-                    receipt.addDiscount(discount);
-            }
+            doHandleOffers(receipt, offers, catalog, p, productQuantities.get(p));
 
         }
     }
+
+    private void doHandleOffers(Receipt receipt, Map<Product, Offer> offers, SupermarketCatalog catalog, Product p, double quantity) {
+        if (offers.containsKey(p)) {
+            Offer offer = offers.get(p);
+            double unitPrice = catalog.getUnitPrice(p);
+            int quantityAsInt = (int) quantity;
+            int x = 1;
+
+            Discount discount1 = null;
+            int x1;
+            if (offer.offerType == SpecialOfferType.ThreeForTwo) {
+                x1 = 3;
+
+                int numberOfXs = quantityAsInt / x1;
+                if (quantityAsInt > 2) {
+                    double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+                    discount1 = new Discount(p, "3 for 2", -discountAmount);
+                }
+            } else {
+                x1 = x;
+                Discount discount11 = null;
+                int x11 = x1;
+
+                if (offer.offerType == SpecialOfferType.TwoForAmount) {
+                        x11 = 2;
+                        double total = offer.argument * (quantityAsInt / x11) + quantityAsInt % 2 * unitPrice;
+                        double discountN = unitPrice * quantity - total;
+                        discount11 = new Discount(p, "2 for " + offer.argument, -discountN);
+                    int numberOfXs = quantityAsInt / x11;
+                } else {
+
+                    if (offer.offerType == SpecialOfferType.FiveForAmount) {
+
+                        Discount discount111 = discount11;
+                        int x111 = x11;
+                        x111 = 5;
+                        int numberOfXs = quantityAsInt / x111;
+                        if (quantityAsInt >= 5) {
+                            double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+                            discount111 = new Discount(p, x111 + " for " + offer.argument, -discountTotal);
+                        }
+                        discount11 = discount111;
+                    } else {
+                        Discount discount111 = discount11;
+                        int x111 = x11;
+                        int numberOfXs = quantityAsInt / x111;
+
+                        if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
+
+                            discount111 = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
+                        }
+
+                        if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+                            double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+                            discount111 = new Discount(p, x111 + " for " + offer.argument, -discountTotal);
+                        }
+                        discount11 = discount111;
+                    }
+                }
+                discount1 = discount11;
+            }
+            if (discount1 != null)
+                receipt.addDiscount(discount1);
+        }
+    }
+
 }
--- java/src/test/java/dojo/supermarket/model/SupermarketTest.java
+++ java/src/test/java/dojo/supermarket/model/SupermarketTest.java
@@ -1,44 +1,61 @@
 package dojo.supermarket.model;
 
 import dojo.supermarket.ReceiptPrinter;
-import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SupermarketTest {
+    public static final Product TOOTHBRUSH = new Product("toothbrush", ProductUnit.Each);
+    public static final Product APPLES = new Product("apples", ProductUnit.Kilo);
 
     // Todo: test all kinds of discounts are applied properly
 
     @Test
     public void tenPercentDiscount() {
-        SupermarketCatalog catalog = new FakeCatalog();
-        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
-        catalog.addProduct(toothbrush, 0.99);
-        Product apples = new Product("apples", ProductUnit.Kilo);
-        catalog.addProduct(apples, 1.99);
-
-        Teller teller = new Teller(catalog);
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
-
-        ShoppingCart cart = new ShoppingCart();
-        cart.addItemQuantity(apples, 2.5);
-        
-        // ACT
-        Receipt receipt = teller.checksOutArticlesFrom(cart);
 
         // ASSERT
-        assertEquals(4.975, receipt.getTotalPrice(), 0.01);
-        assertEquals(Collections.emptyList(), receipt.getDiscounts());
-        assertEquals(1, receipt.getItems().size());
-        ReceiptItem receiptItem = receipt.getItems().get(0);
-        assertEquals(apples, receiptItem.getProduct());
-        assertEquals(1.99, receiptItem.getPrice());
-        assertEquals(2.5*1.99, receiptItem.getTotalPrice());
-        assertEquals(2.5, receiptItem.getQuantity());
+//        assertEquals(4.975, receipt.getTotalPrice(), 0.01);
+//        assertEquals(Collections.emptyList(), receipt.getDiscounts());
+//        assertEquals(1, receipt.getItems().size());
+//        ReceiptItem receiptItem = receipt.getItems().get(0);
+//        assertEquals(apples, receiptItem.getProduct());
+//        assertEquals(price, receiptItem.getPrice());
+//        assertEquals(quantity * price, receiptItem.getTotalPrice());
+//        assertEquals(quantity, receiptItem.getQuantity());
+//
+//        String printReceipt = new ReceiptPrinter().printReceipt(receipt);
+        //Approvals.verify(getReceipt(APPLES,1.99, 2.5, SpecialOfferType.TenPercentDiscount, TOOTHBRUSH, 10.0));
 
+        CombinationApprovals.verifyAllCombinations((product, price, quantity, specialOfferType, specialOfferProduct, specialOfferArgument, numberOfTimestoAdd) -> getReceipt(product, price, quantity, specialOfferType, specialOfferProduct, specialOfferArgument, numberOfTimestoAdd),
+                new Product[]{APPLES},
+                new Double[]{1.99},
+                new Double[]{2.5, 3.0, 5.0, 1.0},
+                new SpecialOfferType[]{SpecialOfferType.TenPercentDiscount, SpecialOfferType.ThreeForTwo, SpecialOfferType.TwoForAmount, SpecialOfferType.FiveForAmount},
+                new Product[]{TOOTHBRUSH, APPLES},
+                new Double[]{10.0},
+                new Integer[]{2, 0});
+
+    }
+
+    private String getReceipt(Product product, double price, double quantity, SpecialOfferType specialOfferType, Product specialOfferProduct, double specialOfferArgument, int numberOfTimestoAdd) {
+        SupermarketCatalog catalog = new FakeCatalog();
+        catalog.addProduct(product, price);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(specialOfferType, specialOfferProduct, specialOfferArgument);
+
+        ShoppingCart cart = new ShoppingCart();
+        for (int i = 0; i < numberOfTimestoAdd; i++) {
+            cart.addItemQuantity(product, quantity);
+
+        }
+
+
+        // ACT
+        String printReceipt = new ReceiptPrinter().printReceipt(teller.checksOutArticlesFrom(cart));
+        return printReceipt;
     }
 
 
